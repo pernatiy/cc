61d6afd703b0ba5f6e3258f23d4747b86a6af043
Denys Sokolovskyi <denys.sokolovskyi@ienso.com>
2024-06-12 20:06:43 +0300

    Enhance notification caption

--- cc.js
+++ cc.js
@@ -260,7 +260,7 @@ Controller.prototype = {
         if (info.buy()) {
             ++this._total;
             success = true;
-            this.notify("autobuy", info.name, info.icon);
+            this.notify("Auto buy", info.name, info.icon);
         }
         Game.buyMode = buy_mode;
         this._target = null;
